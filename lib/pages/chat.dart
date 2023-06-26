de69ba4264ab0e382e3f39339a8c84eecddd1544
AlvinIndrawan <86299016+AlvinIndrawan@users.noreply.github.com>
2023-06-26 16:14:40 +0700

    Create chat.dart
    Add chat page

---
+++ lib/pages/chat.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:chat_bubbles/chat_bubbles.dart';
+
+class Chat extends StatefulWidget {
+  const Chat({Key? key}) : super(key: key);
+
+  @override
+  State<Chat> createState() => _ChatState();
+}
+
+class _ChatState extends State<Chat> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("E-helpDesk"),
+      ),
+      body: Stack(
+        children: [
+          ListView(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              BubbleSpecialThree(
+                text: 'Izin nanya dong',
+                color: Colors.green,
+                tail: true,
+                textStyle: TextStyle(color: Colors.white, fontSize: 16),
+              ),
+              BubbleSpecialThree(
+                text: 'Toilet dimana yah?',
+                color: Colors.green,
+                tail: true,
+                textStyle: TextStyle(color: Colors.white, fontSize: 16),
+              ),
+              BubbleSpecialThree(
+                text: "Lu pikir aja sendiri!!!",
+                color: Color(0xFFE8E8EE),
+                tail: true,
+                isSender: false,
+              ),
+            ],
+          ),
+          MessageBar(
+            onSend: (_) => print(_),
+            actions: [
+              InkWell(
+                child: Icon(
+                  Icons.add,
+                  color: Colors.black,
+                  size: 24,
+                ),
+                onTap: () {},
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 8, right: 8),
+                child: InkWell(
+                  child: Icon(
+                    Icons.camera_alt,
+                    color: Colors.green,
+                    size: 24,
+                  ),
+                  onTap: () {},
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
