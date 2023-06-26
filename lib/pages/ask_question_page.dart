5d13684cc91fa00df7a4f20e4b94d7fe5ec930bf
ink0rr <tomflynnbeltsazar@gmail.com>
2023-06-26 18:43:36 +0700

    Update ask question page

---
+++ lib/pages/ask_question_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class AskQuestionPage extends HookWidget {
+  const AskQuestionPage({super.key});
+
+  final List<String> categories = const [
+    "Materi Perkuliahan",
+    "Siakad",
+    "E-learning",
+    "Jadwal Kuliah",
+    "Biaya Kuliah",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final title = useTextEditingController();
+    final category = useState<String?>(categories.first);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Tanya Help-Desk"),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              children: [
+                DropdownButtonFormField(
+                  decoration: const InputDecoration(
+                    labelText: "Kategori",
+                  ),
+                  items: categories
+                      .map((e) => DropdownMenuItem(
+                            value: e,
+                            child: Text(e),
+                          ))
+                      .toList(),
+                  onChanged: (value) {
+                    category.value = value;
+                  },
+                ),
+                const SizedBox(height: 24),
+                TextFormField(
+                  controller: title,
+                  decoration: const InputDecoration(
+                    labelText: "Judul",
+                  ),
+                ),
+                const SizedBox(height: 24),
+                TextFormField(
+                  controller: title,
+                  decoration: const InputDecoration(
+                    labelText: "Deskripsi",
+                  ),
+                ),
+                const SizedBox(height: 48),
+                ElevatedButton(
+                  child: const Text("Ajukan Pertanyaan"),
+                  onPressed: () {},
+                ),
+                const SizedBox(height: 18),
+                OutlinedButton(
+                  child: const Text("Cancel"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
+import 'ask_question_page.dart';
 import 'login_page.dart';
 
 class HomePage extends HookWidget {
@@ -41,7 +42,11 @@ class HomePage extends HookWidget {
           Icons.add,
           color: Colors.white,
         ),
-        onPressed: () {},
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => const AskQuestionPage(),
+          ));
+        },
       ),
     );
   }
